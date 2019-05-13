60bb5f5fc904f08f1d9ac3dd32a607a1b1d5d29a
Gašper Pust <gapi.pust@gmail.com>
2019-05-13 12:18:29 +0200

    Razred Igra

--- model.py
+++ model.py
@@ -4,3 +4,68 @@ PONOVLJENA_CRKA = "0"
 NAPACNA_CRKA = "-"
 ZMAGA = "W"
 PORAZ = "X"
+class Igra:
+    def __init__(self, beseda, crke=None):
+        geslo = beseda
+        if crke == None:
+            self.crke = []
+        else: 
+            self.crke = crke
+
+    def napacne_crke(self):
+        napacne_crke = []
+        for crka in self.crke:
+            if crka not in self.geslo:
+                napacne_crke.append(crka)
+        return napacne_crke
+
+    def pravilne_crke(self):
+        pravilne_crke = []
+        for crka in self.crke:
+            if crka in self.geslo:
+                pravilne_crke.append(crka)
+        return pravilne_crke
+
+    def stevilo_napak(self):
+        return len(self.napacne_crke())
+    
+    def zmaga(self):
+        for crka in self.geslo:
+            if crka not in self.pravilne_crke():
+                return False
+        return True
+
+    def poraz(self):
+        if self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK:
+            return True
+        else:
+            return False
+    
+    def pravilni_del_gesla(self):
+        pravilni_del = self.geslo
+        for crka in self.geslo:
+            if crka not in self.pravilne_crke():
+                pravilni_del = pravilni_del.replace(crka, "_")
+        return pravilni_del
+    
+    def nepravilni_ugibi(self):
+        nepravilni_ugibi = ""
+        for crka in self.napacne_crke():
+            nepravilni_ugibi += " " + crka
+        return nepravilni_ugibi
+
+    def ugibaj(crka):
+        velika_crka = crka.upper()
+        if velika_crka in self.crke:
+            return PONOVLJENA_CRKA
+        elif velika_crka in self.geslo
+            if self.zmaga():
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA
+        elif velika_crka not in self.geslo:
+            if self.poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
+
